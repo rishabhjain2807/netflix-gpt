a0dbef21e024b9701f39b39d66239cd5849bcdaf
Rishabhjain <jain.rishabh2807@gmail.com>
2023-11-27 14:49:26 +0530

    271123

---
+++ src/components/Login.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import { BG_URL } from "../utils/constants";
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img src={BG_URL} alt="bg-img" />
+      </div>
+
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="text-3xl font-bold py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4" onClick={toggleSignInForm()}>
+          New to Netflix? Sign Up Now
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
